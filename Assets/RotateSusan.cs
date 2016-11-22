c18f2eba2d5b1f5524f40e204584ab82ad94d7d5
shadowfax6894 <anniekelly6894@gmail.com>
2016-11-21 20:09:10 -0500

    basic support for lazy susan, controlled by left/right keys

---
+++ Assets/RotateSusan.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateSusan : MonoBehaviour {
+
+	public AnimationCurve OpenOutCurve;
+	public float SpeedMultiplier;
+	public int numPedestals;
+
+	void Start () {
+	}
+	
+	// eventually replace this with swipe events
+	void Update () {
+        if (Input.GetKey("right")){
+            OpenOut(Vector3.up);
+        }
+        
+        if (Input.GetKey("left")){
+            OpenOut(Vector3.down);
+        }
+	}
+
+	public void OpenOut(Vector3 vector){
+		StartCoroutine(_openOut(vector));
+	}
+
+	IEnumerator _openOut(Vector3 vector){
+		float curveTime = 0f;
+		float curveAmount = OpenOutCurve.Evaluate(curveTime);
+		float rotAmount = 360.0f/numPedestals;
+
+		while (curveAmount < 1.0f){
+			curveTime += Time.deltaTime * SpeedMultiplier;
+			curveAmount = OpenOutCurve.Evaluate(curveTime);
+			transform.Rotate(vector * Time.deltaTime * curveAmount * rotAmount);
+			yield return null;
+		}
+
+	}
+}
