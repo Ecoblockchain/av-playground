ead19d7256b561356d05703846471b63619b7d80
shadowfax6894 <anniekelly6894@gmail.com>
2016-11-30 15:43:58 -0700

    particles respond to number of sequencers selected now because db was fucky

--- Assets/Scripts/ParticleVisualizer.cs
+++ Assets/Scripts/ParticleVisualizer.cs
@@ -3,13 +3,14 @@ using System.Collections;
 
 public class ParticleVisualizer : MonoBehaviour {
 
-	public int sampleSize = 1024;
+	public int sampleSize = 512;
 	public float[] samples;
 	public ParticleSystem particleSystem;
 	private float sum;
 	private float rmsValue;
 	private float refValue = 0.1f;
 	private float dbValue;
+	private int numActiveSteps;
 
 	void Start() {
 		samples = new float[sampleSize];
@@ -17,34 +18,38 @@ public class ParticleVisualizer : MonoBehaviour {
 	}
 
 	void Update(){
-		AudioListener.GetOutputData(samples, 0);
-		for(int i = 0; i< sampleSize ; i++){
-			sum += samples[i]*samples[i];
+		numActiveSteps = 0;
+		Sequencer hihat = GameObject.Find("HiHat").GetComponent<Sequencer>();
+		Sequencer kick = GameObject.Find("Kick").GetComponent<Sequencer>();
+		Sequencer snare = GameObject.Find("Snare").GetComponent<Sequencer>();
+		Sequencer crash = GameObject.Find("Crash").GetComponent<Sequencer>();
+		for (int i = 0; i < 8; i++){
+			if (hihat.sequence[i]) numActiveSteps++;
+			if (kick.sequence[i]) numActiveSteps++;
+			if (snare.sequence[i]) numActiveSteps++;
+			if (crash.sequence[i]) numActiveSteps++;
 		}
-	    rmsValue = Mathf.Sqrt(sum/sampleSize); // rms = square root of average
-	    dbValue = 20*Mathf.Log10(rmsValue/refValue); // calculate dB
-	    Debug.Log(dbValue);
-	    if (dbValue < -160) dbValue = -160; // clamp it to -160dB min
-	   
+		Debug.Log(numActiveSteps);
+
 	    var emission = particleSystem.emission;
-	    if (dbValue <= 40.0f) {
+	    if (numActiveSteps <= 0) {
 	    	particleSystem.Stop();
 	    } else{
 	    	emission.rate = 1000.0f;
 	    	particleSystem.gravityModifier = 0.75f;
-	    	if (dbValue > 40.5f){
+	    	if (numActiveSteps > 0){
 	    		particleSystem.gravityModifier = 0.66f;
 	    	}
-	    	if (dbValue > 42.0f){
+	    	if (numActiveSteps > 4){
 	    		particleSystem.gravityModifier = 0.40f;
 	    	}
-	    	if (dbValue > 43.0f){
+	    	if (numActiveSteps > 10){
 	    		particleSystem.gravityModifier = 0.20f;
 	    	}
-	    	if (dbValue > 45.0f){
+	    	if (numActiveSteps > 16){
 	    		emission.rate = 1000.0f;
 	    		particleSystem.gravityModifier = 0.0f;
-	    	} else if(dbValue > 48.0f){
+	    	} else if(numActiveSteps > 20){
 	    		emission.rate = 2000.0f;
 	    		particleSystem.gravityModifier = 0.0f;
 	    	}
