b8009b28f0b6fdef643d52baab14227c102ddf0c
shadowfax6894 <anniekelly6894@gmail.com>
2016-12-05 11:01:39 -0700

    updated icosphere highligting scripts, needs testing

--- Assets/Scripts/IcosphereHighlighter.cs
+++ Assets/Scripts/IcosphereHighlighter.cs
@@ -2,20 +2,33 @@ using UnityEngine;
 using System.Collections;
 using VRTK;
 
+/* attach to right controller */
 public class IcosphereHighlighter : MonoBehaviour {
-	public GameObject device;
+	private GameObject lastHitByRay;
+	private GameObject nowHitByRay;
 	private VRTK_SimplePointer simplePointer;
 
 	void Start(){
+		simplePointer = gameObject.GetComponent<VRTK_SimplePointer>();
 	}
 
 	void Update(){
-	   // controller = GameObject.Find("Controller (right)");
-	  /*  simplePointer = device.GetComponent<VRTK_SimplePointer>();
-	    Debug.Log(simplePointer);
-	    if (simplePointer.pointerTarget == gameObject){
-	        gameObject.GetComponent<Renderer>().sharedMaterial = gameObject.GetComponent<SequenceChanger>().mat3;
+		nowHitByRay = simplePointer.pointerTarget.gameObject;
+	    if (lastHitByRay && lastHitByRay != nowHitByRay){
+	    	SequenceChanger sequenceChanger;
+	    	if (sequenceChanger = nowHitByRay.GetComponent<SequenceChanger>()){
+	    		nowHitByRay.GetComponent<Renderer>().sharedMaterial = sequenceChanger.mat3; // highlight blue
+	    	}
+	    	else if (sequenceChanger = lastHitByRay.GetComponent<SequenceChanger>()){
+	    		if(sequenceChanger.toggle){
+	    			lastHitByRay.GetComponent<Renderer>().sharedMaterial = sequenceChanger.mat1; // green glow
+	    		}
+	    		else{
+	    			lastHitByRay.GetComponent<Renderer>().sharedMaterial = sequenceChanger.mat2; // default
+	    		}
+	    	}
 	    }
-	*/
+	    lastHitByRay = nowHitByRay;
 	}
+
 }
